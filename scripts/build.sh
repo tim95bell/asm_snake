6929cc2a9c5513815d1499ae7cbac6088cbca8ac
tim95bell <tim95bell@gmail.com>
2024-08-01 16:39:40 +1000

    clean up build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,17 +8,10 @@ BUILD_DIR=./build
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 
-# FLAGS="-Wall -g"
-# SRC_FILES="./src/main.S"
-# clang++ $FLAGS $SRC_FILES -o $BUILD_DIR/snake -I./libs/raylib/include -L./libs/raylib/lib -lraylib -Wl,-framework,OpenGL -Wl,-framework,CoreVideo -Wl,-framework,IOKit -Wl,-framework,Cocoa
-
-PRE_PROCESSED_OUTPUT_DIR=$BUILD_DIR/pre_processed
 OBJECT_FILE_DIR=$BUILD_DIR/object_files
-mkdir -p $PRE_PROCESSED_OUTPUT_DIR
 mkdir -p $OBJECT_FILE_DIR
 
-cpp -g ./src/main.S $PRE_PROCESSED_OUTPUT_DIR/main.S
-as -g -Wall -o $OBJECT_FILE_DIR/main.o $PRE_PROCESSED_OUTPUT_DIR/main.S
+as -g -Wall -o $OBJECT_FILE_DIR/main.o ./src/main.S
 ld -o $BUILD_DIR/snake $OBJECT_FILE_DIR/main.o -L./libs/raylib/lib -lraylib -framework OpenGL -framework CoreVideo -framework IOKit -framework Cocoa -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _main -arch arm64
 
 popd
